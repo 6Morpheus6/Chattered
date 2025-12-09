a3b933f62b470d84a5af67c4a8ca9d3739232beb
6Morpheus6 <wanton-decay@gmx.net>
2025-12-09 06:24:39 +0100

    try intel mac support

--- torch.js
+++ torch.js
@@ -48,6 +48,16 @@ module.exports = {
       },
       "next": null
     },
+    // intel mac
+    {
+      "when": "{{platform === 'darwin' && arch !== 'arm64'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cpu"
+      }
+    },
     // linux nvidia
     {
       "when": "{{platform === 'linux' && gpu === 'nvidia'}}",
